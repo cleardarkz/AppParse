39d3d8f6890954c5bb7f9e5387673278cb1f1512
Aviad <Aviad@AVIADSMAIN>
2022-06-22 11:00:15 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace AppParse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load Chat FIle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(46, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Export";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(80, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 9);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "WhatsApp Chat Parser - Aviad .T";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(207, 121);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "AppParse";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace AppParse
+{
+    public partial class Form1 : Form
+    {
+        List<APUser> users = new List<APUser>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class APUser
+        {
+            public string Name;
+            public List<APMsg> Messages;
+        }
+
+        public class APMsg
+        {
+            public string Date;
+            public string Time;
+            public string Content;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            users = new List<APUser>();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string[] lines = File.ReadAllLines(openFileDialog1.FileName);
+
+                    APMsg lastMessage = null;
+
+                    Regex rx = new Regex(@"(\d{0,2}\/\d{0,2}\/\d{0,4}, \d{0,2}:\d{0,2} - )([^:]*)(.*)", RegexOptions.Multiline | RegexOptions.IgnoreCase);
+                    foreach (string line in lines)
+                    {
+                        var mc = rx.Matches(line);
+                        foreach (Match mcc in mc)
+                        {
+                            GroupCollection groups = mcc.Groups;
+                            var date = groups[1].Value.Replace(" - ", "").Replace(", ", "|");
+                            var sd = date.Split('|');
+                            var sfrom = groups[2].Value;
+                            var smessage = groups[3].Value.Replace(":", "");
+
+                            var user = (from us in users where us.Name == sfrom select us).FirstOrDefault();
+                            if (user != null)
+                            {
+                                var nmsg = new APMsg() { Content = smessage, Date = sd[0], Time = sd[1] };
+                                user.Messages.Add(nmsg);
+                                lastMessage = nmsg;
+                            }
+                            else
+                            {
+                                APUser us = new APUser();
+                                us.Name = sfrom;
+                                us.Messages = new List<APMsg>();
+                                var nmsg = new APMsg() { Content = smessage, Date = sd[0], Time = sd[1] };
+                                us.Messages.Add(nmsg);
+                                lastMessage = nmsg;
+                                users.Add(us);
+                            }
+                        }
+
+                        if (mc.Count == 0)
+                        {
+                            lastMessage.Content += " ** " + line;
+                        }
+                    }
+
+                    button2.Enabled = true;
+                }
+
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Cannot load file, unknown format");
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var count = 0;
+            foreach (var us in users)
+            {
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(us.Name + ".txt"))
+                    {
+                        foreach (var msg in us.Messages)
+                        {
+                            writer.WriteLine(msg.Date + "\t" + msg.Time + "\t" + msg.Content);
+                        }
+
+                        count++;
+                    }
+                }
+
+                catch(Exception ex) { }
+
+
+            }
+
+            MessageBox.Show("Exported " + count + " files successfully");
+            button2.Enabled = false;
+        }
+    }
+}
